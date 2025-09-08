7bc578bf761a17b727a9755e8005cf4d02e31850
mustaqeem-m <mmmustaqeem1910@gmail.com>
2025-09-08 16:00:19 +0530

    Playing with routes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,20 +2,30 @@ const express = require('express');
 
 const app = express();
 
-// app.use((req, res) => {
-//   res.send('Hello welcome to port 1234');
-// });
+//! this '/' routehandler will act like a widlcard means that any route start with / eg.login, express assume that it's this '/' route. express ll check routes by order ,
+//! order of routing is very important when its comes to routing, put handler like '/'  or '' after all routes otherewise all routes ll fall under these route.
 
-app.get('/login', (req, res) => {
-  res.send('welcome to login page');
+app.get('/user', (req, res) => {
+  console.log(req.query);
+  res.send({ firstname: 'Mohammed', lastname: 'Mustaqeem' });
 });
 
-app.get('/profile', (req, res) => {
-  res.send('this is users profile');
+app.post('/user', (req, res) => {
+  // logic to save data in db
+  res.send('data updated in DB successfully!');
+});
+
+app.delete('/user', (req, res) => {
+  //logic to delete data in DB
+  res.send('data deleted in DB successfully!');
 });
 
 app.use('/dashboard', (req, res) => {
   res.send('this is dashboard of devtinder');
+});
+
+app.use('/', (req, res) => {
+  res.send('Hello welcome to port 1234');
 });
 
 app.listen(2222, () => {
