afb9b404c260b9859ebc9906e531480342167cf0
mustaqeem-m <mmmustaqeem1910@gmail.com>
2025-09-08 19:52:58 +0530

    multiple route handlers - testing

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,28 +5,27 @@ const app = express();
 //! this '/' routehandler will act like a widlcard means that any route start with / eg.login, express assume that it's this '/' route. express ll check routes by order ,
 //! order of routing is very important when its comes to routing, put handler like '/'  or '' after all routes otherewise all routes ll fall under these route.
 
-app.get('/user', (req, res) => {
-  console.log(req.query);
-  res.send({ firstname: 'Mohammed', lastname: 'Mustaqeem' });
-});
-
-app.post('/user', (req, res) => {
-  // logic to save data in db
-  res.send('data updated in DB successfully!');
-});
-
-app.delete('/user', (req, res) => {
-  //logic to delete data in DB
-  res.send('data deleted in DB successfully!');
-});
-
-app.use('/dashboard', (req, res) => {
-  res.send('this is dashboard of devtinder');
-});
-
-app.use('/', (req, res) => {
-  res.send('Hello welcome to port 1234');
-});
+app.use(
+  (req, res, next) => {
+    next();
+    // res.send('your request just hit the port 2222');
+  },
+  (req, res, next) => {
+    // res.send('hello there what are you looking at this port 2:)');
+    next();
+  },
+  (req, res, next) => {
+    // res.send('hello there what are you looking at this port 3:)');
+    next();
+  },
+  (req, res, next) => {
+    // res.send('hello there what are you looking at this port 4:)');
+    next();
+  },
+  (req, res, next) => {
+    res.send('hello there what are you looking at this port 5:)');
+  }
+);
 
 app.listen(2222, () => {
   console.log('server is listening to the port 2222');
